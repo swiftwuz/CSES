45f8c86eadbee11dab0343902d9aa1da38be703a
Nana Kojo Ewusie <enanakojo77@outlook.com>
2020-07-14 19:19:15 +0000

    Approach to Problem Sets.

---
+++ missing_number.py
@@ -0,0 +1,9 @@
+try:
+    def missing_number():
+        num = int(input())
+        result = num*(num+1)//2 - sum(map(int, input().split()))
+        return result
+
+    print(missing_number())
+except EOFError as e:
+    print(end="")
---
+++ weird_algorithm.py
@@ -0,0 +1,12 @@
+def weird():
+    n = int(input())
+    while n != 1:
+        print(n, '', end='')
+        if n % 2 == 0:
+            n = (n//2)
+        elif n % 2 == 1:
+            n = (n * 3) + 1
+    return n
+
+
+print(weird())
