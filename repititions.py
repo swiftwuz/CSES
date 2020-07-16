8d8eed54fa34d57e171af020e591d9b44d4808bf
Nana Kojo Ewusie <enanakojo77@outlook.com>
2020-07-16 16:28:35 +0000

    Solution.

--- repititions.py
+++ repititions.py
@@ -1,15 +1,15 @@
 def repitition():
-    s = str(input())
+    sequence = str(input())
     maximum = 0
     count = 0
     current = ""
 
-    for c in s:
-        if c == current:
+    for char in sequence:
+        if char == current:
             count += 1
         else:
             count = 1
-            current = c
+            current = char
         maximum = max(count, maximum)
     return maximum
 
